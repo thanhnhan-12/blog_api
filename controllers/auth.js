412f13ec1e29c82d4d995fbb9e61d05109329382
unknown <nguyenthanhnhan12701@gmail.com>
2023-07-15 19:04:02 +0700

    Fecth Data

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 import { db } from '../db.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const register = (req, res) => {
+  // Check existing user
+
   const q = 'Select * from user where email = ? or username = ? ';
 
   db.query(q, [req.body.email, req.body.name], (err, data) => {
@@ -23,6 +26,41 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  // Check user
 
-export const logout = (req, res) => {};
+  const q = 'Select * from user where username = ?';
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.json(err);
+
+    if (data.length === 0) return res.status(404).json('User not found!');
+
+    // Check password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password,
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json('Wrong username or password!');
+
+    const token = jwt.sign({ id: data[0].id }, 'jwtkey');
+
+    const { password, ...other } = data[0];
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
+
+export const logout = (req, res) => {
+  res.clearCookie("access_token", {
+    sameSite: "none",
+    secure: true
+  }).status(200).json("User has been logged out.")
+};
--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,29 @@
+import { db } from '../db.js';
+
+export const getPosts = (req, res) => {
+  const q = req.query.cat
+    ? 'Select * from posts where cat=?'
+    : 'Select * from posts';
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if(err) return res.send(err);
+
+    return res.status(200).json(data);
+  })
+};
+
+export const getPost = (req, res) => {
+  res.json('dssdf');
+};
+
 export const addPost = (req, res) => {
+  res.json('dssdf');
+};
+
+export const deletePost = (req, res) => {
+  res.json('dssdf');
+};
+
+export const updatePost = (req, res) => {
   res.json('dssdf');
 };
