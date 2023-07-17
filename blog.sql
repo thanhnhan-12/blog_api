19aa4087f4cdce1caedb49fd9b511ad570995cb4
unknown <nguyenthanhnhan12701@gmail.com>
2023-07-17 15:12:07 +0700

    Update sql

--- blog.sql
+++ blog.sql
@@ -15,3 +15,17 @@ Create table user
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'nhan';
 
+/*
+Create table posts
+(
+	id int auto_increment primary key,
+    title varchar(255) not null,
+    `desc` varchar(1000) not null,
+    img varchar(255) not null,
+    date datetime,
+    uid int,
+    cat varchar(45),
+    foreign key (uid) references user(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+*/
+
