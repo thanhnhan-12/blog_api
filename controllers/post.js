145f5276ffaccaca892e813da5b1b4ebfd12d008
unknown <nguyenthanhnhan12701@gmail.com>
2023-07-17 15:01:57 +0700

    Update post

--- controllers/post.js
+++ controllers/post.js
@@ -15,7 +15,7 @@ export const getPosts = (req, res) => {
 
 export const getPost = (req, res) => {
   const q =
-    'SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM user u JOIN posts p ON u.id = p.uid WHERE p.id = ? ';
+    'SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM user u JOIN posts p ON u.id = p.uid WHERE p.id = ? ';
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -25,7 +25,29 @@ export const getPost = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-  res.json('dssdf');
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json('Not authenticated!');
+
+  jwt.verify(token, 'jwtkey', (err, userInfo) => {
+    if (err) return res.status(403).json('Token is not valid!');
+
+    const q =
+      'Insert into posts(`title`, `desc`, `img`, `cat`, `date`, `uid`) Values(?) ';
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+      req.body.date,
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json('Post has been created.');
+    });
+  });
 };
 
 export const deletePost = (req, res) => {
@@ -48,5 +70,22 @@ export const deletePost = (req, res) => {
 };
 
 export const updatePost = (req, res) => {
-  res.json('dssdf');
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json('Not authenticated!');
+
+  jwt.verify(token, 'jwtkey', (err, userInfo) => {
+    if (err) return res.status(403).json('Token is not valid!');
+
+    const postId = req.params.id;
+    
+    const q =
+      'Update posts set `title` = ?,`desc` = ?,`img` = ?,`cat` = ? WHERE `id` = ? and `uid` = ?';
+
+    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json('Post has been updated.');
+    });
+  });
 };
